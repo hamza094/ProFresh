885ca1e5389f5466cd5d0b37525f3aa53126c1d0
hamza <hamza_pisces@live.com>
2021-05-24 15:53:03 +0500

    Design main welcome page

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -103,11 +103,11 @@
                                 <!-- Authentication Links -->
                                 @guest
                                     <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Sign In') }}</a>
                                     </li>
                                     @if (Route::has('register'))
                                         <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
                                         </li>
                                     @endif
                                 @else
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('welcome');
+    return view('main.home');
 });
 
 Auth::routes();
