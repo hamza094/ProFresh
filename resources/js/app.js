350b22ade4334eabc4d1be461eaa02d9c641f8e7
hamza094 <hamza_pisces@live.com>
2023-01-30 21:36:34 +0500

    git commit --amend --no-edit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
 
 import Vue from 'vue';
@@ -11,64 +17,68 @@ import "emoji-mart-vue-fast/css/emoji-mart.css";
 import alertNotice from './mixins/alertNotice';
 import currentStage from './mixins/currentStage';
 import conversation from './mixins/conversation';
-import Datetime from 'vue-datetime/dist/vue-datetime.css';
-import 'vue-datetime/dist/vue-datetime.css';
 import Chart from 'chart.js';
-import { Settings } from 'luxon';
-import VueUploadComponent from 'vue-upload-component';
-import VueProgressBar from 'vue-progressbar';
-import router from './router.js'
-import store from "./store";
+import 'animate.css';
+import "cropperjs/dist/cropper.css"
+import swal from 'sweetalert2';
+import VueToastify from "vue-toastify";
+import { Datetime } from 'vue-datetime';
+// You need a specific loader for CSS files
+import 'vue-datetime/dist/vue-datetime.css';
+import { Settings } from 'luxon'
+
 
 Vue.use(Vuex);
 Vue.use(VueBus);
 Vue.use(VTooltip);
 Vue.use(VueSlideoutPanel);
 Vue.use(VModal);
+Vue.use(Datetime)
+
+Vue.component('datetime', Datetime);
+const VueUploadComponent = require('vue-upload-component')
+Vue.component('file-upload', VueUploadComponent)
+
+window.momenttz = require('moment-timezone');
+window.moment = require('moment');
+window.swal=swal;
+
+import router from './router.js'
+import store from "./store";
 
 Vue.mixin(alertNotice);
 Vue.mixin(currentStage);
 Vue.mixin(conversation);
 
-Vue.filter('time', function(data) {
-  return moment(data).format('h:mm:ss a');
-});
 
-Vue.filter('date', function(data) {
-  return moment(data).format("MMM Do YY");
-});
+Vue.filter('time',function(data){
+   return  moment(data).format('h:mm:ss a');
+})
 
-Vue.filter('datetime', function(data) {
-  return moment(data).format("MMM Do YY h:mm:ss a");
-});
+Vue.filter('date',function(data){
+   return  moment(data).format("MMM Do YY");
+})
 
-window.moment = require('moment');
-window.momenttz = require('moment-timezone');
+Vue.filter('datetime',function(data){
+   return  moment(data).format("MMM Do YY h:mm:ss a");
+})
 
-import 'animate.css';
-import "cropperjs/dist/cropper.css"
 
-import swal from 'sweetalert2';
-window.swal = swal;
-
-import VueToastify from "vue-toastify";
 Vue.use(VueToastify, {
-  position: "bottom-left",
-  theme: "light",
-  successDuration: 2050,
-  warningInfoDuration: 2050,
-  errorDuration: 2050,
-  canPause: false
+    position:"bottom-left",
+    theme:"light",
+    successDuration:2050,
+    warningInfoDuration:2050,
+    errorDuration:2050,
+    canPause:false
 });
 
-Vue.use(Datetime);
-Vue.component('datetime', Datetime);
 
-Settings.defaultLocale = 'en';
+//set to display dates for English language
+Settings.defaultLocale = 'en'
 
-Vue.use(VueUploadComponent);
-Vue.component('file-upload', VueUploadComponent);
 
+import VueProgressBar from 'vue-progressbar'
 const options = {
   color: '#bffaf3',
   failedColor: '#874b4b',
@@ -83,8 +93,7 @@ const options = {
   location: 'top',
   inverse: false
 }
-
-Vue.use(VueProgressBar, options);
+Vue.use(VueProgressBar, options)
 
 const components = [    ['project-button', './components/ProjectButton.vue'],
   ['project-form', './components/ProjectForm.vue'],
